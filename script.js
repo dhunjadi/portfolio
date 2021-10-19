d09998463861259c56b2bd2fd2c2d61917363a53
Dario Hunjadi <dario.hunjadi@hotmail.com>
2021-10-19 17:50:21 +0200

    added more javascript and tweaked css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Adding 'active' class to the href of the current section */
+// Adding 'active' class to the href of the current section 
 const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll',  () => {
@@ -17,7 +17,7 @@ window.addEventListener('scroll',  () => {
     })
 })
 
-/* Adding shadow to header when scrolled down */
+// Adding shadow to header when scrolled down 
 window.addEventListener('scroll', () => {
     const header = document.getElementById('header')
     if (this.scrollY >= 80) {
@@ -27,7 +27,7 @@ window.addEventListener('scroll', () => {
     }
 })
 
-/* Opening project images in modal */
+// Opening project images in modal 
 const modal = document.querySelector(".modal")
 const imgs = document.querySelectorAll(".project-img")
 const modalImg = document.querySelector(".modal-img")
@@ -35,6 +35,7 @@ const modalImg = document.querySelector(".modal-img")
 imgs.forEach(img => {
     img.addEventListener('click', () => {
        modal.classList.add("open")
+       modalImg.classList.add("open")
        // Show clicked image
        const imgSrc = img.getAttribute("src")
        modalImg.src = `${imgSrc}`
@@ -44,5 +45,30 @@ imgs.forEach(img => {
 modal.addEventListener('click', (e) => {
     if(e.target.classList.contains("modal")){
         modal.classList.remove("open")
+        modalImg.classList.remove("open")
     }
+})
+
+// Fading animations
+const skillIcons = document.querySelectorAll(".skills-icon")
+const  projectContainers = document.querySelectorAll(".project-container")
+const projectImgs = document.querySelectorAll(".project-container .img-container")
+const projectTxts = document.querySelectorAll(".project-container .project-text-container")
+
+window.addEventListener("scroll", () => {
+    skillIcons.forEach(icon => {
+        let iconPosition = icon.getBoundingClientRect().top
+        if(iconPosition < 750){
+            icon.classList.remove("fade-in")
+        }
+    })
+
+    projectContainers.forEach(container => {
+        let containerPosition = container.getBoundingClientRect().top
+        if(containerPosition < 750) {
+            container.children[0].classList.remove("fade-in-right")
+            container.children[1].classList.remove("fade-in-left")
+        }
+    })
+    
 })
